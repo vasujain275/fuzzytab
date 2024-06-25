9d1fc494fc07a9920483e4e353396d27f2f0604a
vasujain275 <vasujain275@gmail.com>
2024-06-25 15:03:09 +0530

    Init

---
+++ src/entrypoints/newtab/App.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1 class="text-sky-500">Hello</h1>
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{svelte,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        sec: "#23252b",
+        pri: "#000000",
+        offwhite: "#d1cdc7",
+      },
+      fontFamily: {
+        typeface: ["Lato", '"Helvetica Neue"', "helvetica", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
