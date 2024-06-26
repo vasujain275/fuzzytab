1c93c6f88b9e73cff2328f4b2cf64c58bd88e2aa
vasujain275 <vasujain275@gmail.com>
2024-06-26 18:28:59 +0530

    feat: added top layout for settings and search

--- src/entrypoints/newtab/bookmarks.ts
+++ src/entrypoints/newtab/bookmarks.ts
@@ -1,34 +1,53 @@
 import { writable } from "svelte/store";
 import { browser } from "wxt/browser";
 
-export const urls = writable<any[]>([]);
+type BookmarkType = {
+  id: string;
+  title: string;
+  index: number;
+  dateAdded: number;
+  type: "bookmark";
+  url: string;
+  parentId: string;
+};
+
+export const bookmarksArray = writable<BookmarkType[]>([]);
 export const errorMessage = writable<string | null>(null);
 
-export async function fetchBookmarks() {
+async function fetchBookmarks() {
   try {
     const bookmarks = await browser.bookmarks.getTree();
-
-    function extractBookmarksWithUrl(bookmarkNode: any[]): any[] {
-      let bookmarksWithUrl: any[] = [];
-
-      function recurse(node: any) {
-        if (node.url) {
-          bookmarksWithUrl.push(node);
-        }
-        if (node.children) {
-          node.children.forEach((child: any) => recurse(child));
-        }
-      }
-
-      bookmarkNode.forEach((node: any) => recurse(node));
-      return bookmarksWithUrl;
-    }
-
-    const bookmarksWithUrls = extractBookmarksWithUrl(bookmarks[0].children);
-    urls.set(bookmarksWithUrls);
+    const bookmarksWithUrls: any[] | undefined = extractBookmarksWithUrl(
+      bookmarks[0].children,
+    );
+    bookmarksArray.set(bookmarksWithUrls);
     console.log(bookmarksWithUrls);
   } catch (error: any) {
     errorMessage.set(`Failed to fetch bookmarks: ${error.message}`);
     console.error(errorMessage, error);
   }
 }
+
+function extractBookmarksWithUrl(
+  bookmarkNode: any[] | undefined,
+): BookmarkType[] {
+  const bookmarksWithUrl: BookmarkType[] = [];
+
+  function recurse(node: any) {
+    if (node.url) {
+      bookmarksWithUrl.push(node as BookmarkType);
+    }
+    if (node.children) {
+      node.children.forEach(recurse);
+    }
+  }
+
+  if (bookmarkNode) {
+    bookmarkNode.forEach(recurse);
+    return bookmarksWithUrl;
+  } else {
+    return [];
+  }
+}
+
+export { fetchBookmarks };
--- src/entrypoints/newtab/lib/SearchCard.svelte
+++ src/entrypoints/newtab/lib/SearchCard.svelte
@@ -1,21 +1,33 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { urls, errorMessage, fetchBookmarks } from "../bookmarks";
-
+  import SearchIcon from "../../../assets/settings.svg";
+  import { bookmarksArray, errorMessage, fetchBookmarks } from "../bookmarks";
   onMount(() => {
     fetchBookmarks();
   });
 </script>
 
-<main class="p-6 w-full h-full">
-  <h1 class="text-sky-500">Hello</h1>
-  {#if $errorMessage}
-    <p class="text-red-500">{$errorMessage}</p>
-  {:else}
-    <ul>
-      {#each $urls as bookmark}
-        <li>{bookmark.title}</li>
-      {/each}
-    </ul>
-  {/if}
+<main class="p-6 w-full h-full grid grid-rows-12">
+  <div class="row-span-2">
+    <div class="w-full h-9 grid grid-cols-12 text-white">
+      <div class="col-span-1 p-4 flex justify-center items-center">
+        <img src={SearchIcon} alt="search" />
+      </div>
+      <div class="col-span-10 bg-zinc-500"></div>
+      <div class="col-span-1 flex justify-center items-center">
+        <h1>0</h1>
+      </div>
+    </div>
+  </div>
+  <div class="row-span-10">
+    {#if $errorMessage}
+      <p class="text-red-500">{$errorMessage}</p>
+    {:else}
+      <ul>
+        {#each $bookmarksArray as bookmark}
+          <li>{bookmark.title}</li>
+        {/each}
+      </ul>
+    {/if}
+  </div>
 </main>
