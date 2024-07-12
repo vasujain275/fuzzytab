f5978eeaa0c6c66b6ef31ebef626458d15d16c73
vasujain275 <vasujain275@gmail.com>
2024-07-13 01:52:28 +0530

    feat: added eslint

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,20 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   manifest: {
-    permissions: ["bookmarks", "storage", "tabs", "menus", "browserSettings"],
+    permissions: [
+      "bookmarks",
+      "storage",
+      "tabs",
+      "menus",
+      "browserSettings",
+      "cookies",
+      "history",
+    ],
+    browser_specific_settings: {
+      gecko: {
+        id: "fuzzytab@vasujain.me",
+      },
+    },
   },
   srcDir: "src",
   modules: ["@wxt-dev/module-svelte"],
